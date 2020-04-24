c443a8d8ef2ebca05d02fc51bdb37bae45ed00b6
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-04-24 00:01:36 -0300

    Update project - index.js

--- module-01-challenge-01/index.js
+++ module-01-challenge-01/index.js
@@ -1,34 +1,75 @@
-/* eslint-disable no-console */
 /* eslint-disable eqeqeq */
+/* eslint-disable no-console */
 const express = require('express');
 
 const server = express();
 
+/**
+ * Sets JSON as the standard API response.
+ */
 server.use(express.json());
 
+/**
+ * Initialized project array;
+ * Structure of a new project:
+ * [
+ *  {
+ *    id: "1",
+ *    title: "Novo projeto",
+ *    tasks: ["Nova tarefa"]
+ *  }
+ * ];
+ */
 const projects = [];
 
+/**
+ * Middleware checks if the projects exists.
+ */
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
-
   const project = projects.find((p) => p.id == id);
 
   if (!project) {
-    return res.status(400).json({ error: 'Project not found' });
+    return res.status(400).send({ error: 'Project not found.' });
   }
 
   return next();
 }
 
+/**
+ * Middleware requests counter.
+ */
 function logRequests(req, res, next) {
-  console.count('Request number.');
+  console.count('Requests number.');
   return next();
 }
 
-server.get('/projects', (req, res) => res.json(projects));
+server.use(logRequests);
 
+/**
+ * Returns all projects.
+ */
+server.get('/projects', (req, res) => res.status(200).send(projects));
+
+
+/**
+ * Route params: id;
+ * Returns the project with id in the route parameters.
+ */
+server.get('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const project = projects.find((p) => p.id == id);
+
+  return res.status(200).send(project);
+});
+
+/**
+ * Request body: id, title;
+ * Add new project.
+ */
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
+
   const project = {
     id,
     title,
@@ -37,9 +78,14 @@ server.post('/projects', (req, res) => {
 
   projects.push(project);
 
-  return res.json(project);
+  return res.status(201).send(project);
 });
 
+/**
+ * Route params: id;
+ * Request body: title;
+ * Update the project title using the present route parameters of the ID.
+ */
 server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -48,9 +94,13 @@ server.put('/projects/:id', checkProjectExists, (req, res) => {
 
   project.title = title;
 
-  return res.json(project);
+  return res.status(201).send(project);
 });
 
+/**
+ * Route params: id;
+ * Removes a project with an id in the route parameters.
+ */
 server.delete('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
 
@@ -58,9 +108,14 @@ server.delete('/projects/:id', checkProjectExists, (req, res) => {
 
   projects.splice(projectIndex, 1);
 
-  return res.send();
+  return res.status(204).send();
 });
 
+/**
+ * Route params: id;
+ * Request body: title;
+ * Add a new task to the existing project with the associated id.
+ */
 server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -69,9 +124,7 @@ server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
 
   project.tasks.push(title);
 
-  return res.json(project);
+  return res.status(201).send(project);
 });
 
-server.use(logRequests);
-
-server.listen(3000, () => { console.log('server running'); });
+server.listen(3000);
