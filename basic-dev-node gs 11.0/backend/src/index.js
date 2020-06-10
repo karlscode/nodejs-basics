59cca1172d3eecafad65a68a9cd5f84c54e0f97a
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-06-10 18:18:08 -0300

    feat: Add URI GET /projects.

--- basic-dev-node gs 11.0/backend/src/index.js
+++ basic-dev-node gs 11.0/backend/src/index.js
@@ -5,9 +5,19 @@ const app = express()
 
 app.use(express.json())
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  return response.json({status: "OK!"})
+  const { title } = request.query
+
+  const results = title 
+    ? projects.filter(project => project.title.inlucdes(title))
+    : projects
+
+  return response.json(results)
 })
+
+
 
 app.listen(3333, () => {
   console.log('Back-end starded!')
