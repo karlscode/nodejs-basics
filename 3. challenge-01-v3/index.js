bfeb6bd4286a80f8c5449bb5db7ae1a4d1e3e7e5
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-07-01 00:38:30 -0300

    feat: Add challenge-01-v3 project.

---
+++ 3. challenge-01-v3/index.js
@@ -0,0 +1,93 @@
+/* eslint-disable eqeqeq */
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+const projects = []
+/**
+ * Middleware
+ */
+function checkProjectExists (req, res, next) {
+  const { id } = req.params
+
+  if (!isValid(id)) {
+    return res.status(400).send({ error: 'Project not found.' })
+  }
+
+  return next()
+}
+
+function logRequests (req, res, next) {
+  console.count('Request number')
+
+  return next()
+}
+
+server.use(logRequests)
+
+function isValid (idParams) {
+  const project = projects.find(p => p.id == idParams)
+  return project
+}
+
+/**
+ * Endpoints API.
+ */
+
+server.get('/projects', (req, res) => res.status(200).send(projects))
+
+server.get('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params
+
+  const project = isValid(id)
+
+  return res.status(200).send(project)
+})
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project)
+
+  return res.status(201).send(project)
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+  const project = isValid(id)
+
+  project.title = title
+
+  return res.status(200).send(project)
+})
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const projectIndex = projects.findIndex(p => p.id == id)
+
+  projects.splice(projectIndex, 1)
+
+  return res.status(204).send()
+})
+
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = isValid(id)
+
+  project.tasks.push(title)
+
+  return res.status(201).send(project)
+})
+
+server.listen(3000)
